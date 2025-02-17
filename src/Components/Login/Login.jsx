030bb55fdf6d99d455b7c5d41b937619d6b8c0e9
Sérgio Alencar <sergiofalencar@gmail.com>
2025-02-17 13:48:48 -0300

    correcao de extensao

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -16,11 +16,11 @@ const Login = () => {
     <section className={styles.login}>
       <div className={styles.forms}>
         <Routes>
-          <Route path='dogs' element={<LoginForm />} />
+          <Route path='dogs' end element={<LoginForm />} />
           <Route path='dogs/criar' element={<LoginCreate />} />
           <Route path='dogs/perdeu' element={<LoginPasswordLost />} />
           <Route path='dogs/resetar' element={<LoginPasswordReset />} />
-          {/* <Route path='dogs/*' element={<NotFound />} /> */}
+          <Route path='dogs/*' element={<NotFound />} />
         </Routes>
       </div>
     </section>
