2790402189bd49b05c294280817a33bc236f9731
Sérgio Alencar <sergiofalencar@gmail.com>
2025-02-17 13:41:17 -0300

    correcao de extensao

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
-        <Link className={styles.logo} to='/' aria-label='Dogs - Home'>
+        <Link className={styles.logo} to='dogs' aria-label='Dogs - Home'>
           <Dogs />
         </Link>
         {data ? (
