db83eb96f2be1066a7d41cf0632d0f73266f100f
Sérgio Alencar <sergiofalencar@gmail.com>
2025-02-17 15:48:13 -0300

    correcao de extensao

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { HashRouHashRouter, ter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
@@ -15,7 +15,7 @@ import NotFound from './Components/NotFound';
 function App() {
   return (
     <div className='App'>
-      <HashRouter basename='/dogs'>
+      <BrowserRouter basename='/dogs'>
         <UserStorage>
           <Header />
           <main className='AppBody'>
@@ -37,7 +37,7 @@ function App() {
           </main>
           <Footer />
         </UserStorage>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
